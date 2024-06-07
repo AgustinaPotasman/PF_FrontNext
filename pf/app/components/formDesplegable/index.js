b6692cf38360cb3a12e430ac652f595720bbc341
Valenkra <47866545@est.ort.edu.ar>
2024-06-07 10:40:41 -0300

    buscando el error

--- pf/app/components/formDesplegable/index.js
+++ pf/app/components/formDesplegable/index.js
@@ -1,17 +1,17 @@
 import styles from "./formDesplegable.module.css"
-import Boton from "../boton";
 
-export default function formDesplegable({params}){
-    return(
+export default function formDesplegable({}){
+    return (
         <form action="/submit" method="post">
-            <label for="opciones">Opciones:</label>
+            <label for="opciones">Tipo de guardia:</label>
             <select id="opciones" name="opciones">
-                <option value="opcion1">Opción 1</option>
-                <option value="opcion2">Opción 2</option>
-                <option value="opcion3">Opción 3</option>
-                <option value="opcion4">Opción 4</option>
+                <option value="opcion1">Pediatria</option>
+                <option value="opcion2">Ginecología</option>
+                <option value="opcion3">Neonatología</option>
+                <option value="opcion4">Cardiología</option>
+                <option value="opcion4">Otorrinolaringología</option>
+                <option value="opcion4">Cirugía</option>
             </select>
-            <Boton sendText={"Siguiente"}></Boton>
         </form>
     )
 }
--- pf/app/informacion/page.js
+++ pf/app/informacion/page.js
@@ -2,12 +2,13 @@
 import Titulo from "../components/titulo";
 import Input from "../components/input";
 import styles from "./page.module.css";
-
+import formDesplegable from "../components/formDesplegable";
 export default function home() {
   return (
     <main>
         <Titulo params="Informacion sobre el turno!"></Titulo>
         <Input iType="text" iPlaceholder="Ingrese sus sintomas"></Input>
+        <formDesplegable></formDesplegable>
     </main>
   );
 }
