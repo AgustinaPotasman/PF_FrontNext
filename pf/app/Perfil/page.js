871b248f8397c83abce10fae15eb64caa15eebef
Tu Nombre <tuemail@example.com>
2024-06-24 09:58:50 -0300

    actualizado

--- pf/app/Perfil/page.js
+++ pf/app/Perfil/page.js
@@ -1,3 +1,25 @@
+'use client';
+
+import React, { useState } from 'react';
+import Titulo from '../components/titulo';
+import Input from '../components/input';
+import styles from './page.module.css';
+import FormDesplegable from '../components/formDesplegable';
+import Boton from '../components/boton';
+import Modal from '../components/Modal';
+
 export default function Home() {
- 
+    return (
+        <div>
+            <ProximoTurnoPage />
+        </div>
+    );
 }
+
+const ProximoTurnoPage = () => {
+    return (
+        <main className={styles.container}>
+            <Titulo params="Proximo Turno" />
+        </main>
+    );
+};
--- pf/app/ProximoTurno/page.js
+++ pf/app/ProximoTurno/page.js
@@ -1,3 +1,19 @@
+'use client';
+
+import React, { useState } from 'react';
+import Titulo from '../components/titulo';
+import Input from '../components/input';
+import styles from './page.module.css';
+import formDesplegable from '../components/formDesplegable';
+import Boton from '../components/boton';
+import Modal from '../components/Modal';
+
 export default function Home() {
- 
+    const ProximoTurnoPage = () => {
+        return (
+            <main className={styles.container}>
+                <Titulo params="Proximo Turno" />
+            </main>
+        );
+      };
 }
