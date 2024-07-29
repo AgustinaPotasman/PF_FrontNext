ac8cbc1384853579d7a51d3646f17a6ec528eef3
Android Studio <devandroid@ort>
2024-07-29 12:14:27 -0300

    timer hecho no terminado

--- pf/app/components/formDesplegable/index.js
+++ pf/app/components/formDesplegable/index.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 import Boton from "../boton";
-import styles from "./formDesplegable.module.css"; // Importa los estilos aquí
+import styles from "./formDesplegable.module.css"; 
 import axios from "axios";
 
 export default function FormDesplegable() {
   const [categorias, setCategorias] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3000/api/areas")
+    axios.get("http://localhost:3001/api/areas")
       .then(response => {
         setCategorias(response.data); 
       })
@@ -18,7 +18,6 @@ export default function FormDesplegable() {
 
   return (
     <div className={styles.dropdown}>
-      <Boton>Dropdown button</Boton>
       <form action="/submit" method="post">
         <select id="opciones" name="opciones">
           {categorias.map(cat =>  
--- pf/app/components/timer/index.js
+++ pf/app/components/timer/index.js
@@ -1,13 +1,29 @@
-import React from 'react';
 import styles from './timer.module.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Timer() {
+    const [timer, setTimer] = useState([]);
+    let turnosPrevios;
+
+   
+
+    useEffect(() => {
+        axios.get("http://localhost:3001/api/areas")
+        .then(response => {
+            setTimer(response.data);
+        })
+        .catch(error => {
+            console.error('Error fetching:', error);
+            });
+        }, []); 
+
     return (
         <div className={styles.timer}>
-            <div className={styles.circle}>
-                <span className={styles.time}>45</span>
-                <span className={styles.minutes}>minutos</span>
-            </div>
+            {timer.map(time =>  
+            <p key={time.id}>{time.TiempoEsperaAprox*2}</p>
+            )}
         </div>
-    );
+      );
 }
+
