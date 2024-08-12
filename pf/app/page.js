91b07761a3e61916af01cfa83b614f516c497069
Android Studio <devandroid@ort>
2024-08-12 12:11:42 -0300

    actualizar home

--- pf/app/page.js
+++ pf/app/page.js
@@ -1,16 +1,56 @@
-import styles from './page.module.css'
-import Titulo from "./components/titulo";
-import Boton from "./components/boton";
-import Subtitulo from './components/subtitulo/page';
-import Link from "next/link";
-export default function Home() {
+'use client';
+
+import { useEffect, useState } from 'react';
+import styles from './page.module.css'; 
+import Image from 'next/image';
+import Boton from './components/boton';
+
+const HomePage = () => {
+  const [showLogo, setShowLogo] = useState(true);
+
+  useEffect(() => {
+    // Temporizador para ocultar el logo después de 5 segundos
+    const timer = setTimeout(() => {
+      setShowLogo(false);
+    }, 5000);
+
+    // Limpiar el temporizador si el componente se desmonta
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <main className={styles.main}>
-      <Titulo params="Reserva un turno turnos medicos!"></Titulo>
-      <Subtitulo subtitulo="Te asesoramos y atendemos en cualquier inquitud que tengas."></Subtitulo>
+    <div>
+      {showLogo ? (
+        <div className={styles.logoContainer}>
+          <Image
+            src="/img/LOGO.jpg"
+            alt="Logo"
+            layout="fill" // Ajusta el tamaño de la imagen al contenedor
+            objectFit="contain" // Mantiene la proporción de la imagen
+          />
+        </div>
+      ) : (
+        <div className={styles.homeContent}>
+          {/* Aquí va el contenido principal de la página de inicio */}
+          <h1>Bienvenido a la Hospiturn</h1>
+          <h4>Reserva tu turno medico"</h4>
+        </div>
+        
+      )}
       <div className={styles.container}>
-        <Link href="/informacion"><Boton sendText={"¡Empeza aca!"} type={"secondary"}></Boton></Link>
+        <link href="/informacion"><Boton sendText={"¡Empeza aca!"} type={"secondary"}></Boton></link>
       </div>
-    </main>
+    </div>
   );
-}
+};
+
+export default HomePage;
+
+
+
+      <img
+  src="/img/LOGO.jpg"
+  alt="Logo"
+  style={{ width: '360px', height: 'auto' }} 
+/>
+      
