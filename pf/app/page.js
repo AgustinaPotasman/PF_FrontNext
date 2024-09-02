70fb51a463bd205362b6444f2479bbcc4661ca58
Android Studio <devandroid@ort>
2024-09-02 10:20:08 -0300

    actualizado

--- pf/app/page.js
+++ pf/app/page.js
@@ -3,90 +3,63 @@
 import { useEffect, useState } from 'react';
 import styles from './page.module.css'; 
 import Image from 'next/image';
-import Boton from './components/boton';
 import Link from 'next/link';
 
 const HomePage = () => {
-  const [showContent, setShowContent] = useState(false);
-  const [showSecondaryScreen, setShowSecondaryScreen] = useState(false); // Estado para la segunda pantalla
   const [showMap, setShowMap] = useState(false); // Estado para mostrar el mapa
-
-  useEffect(() => {
-    const timerContent = setTimeout(() => {
-      setShowContent(true);
-    }, 500); 
-
-    return () => {
-      clearTimeout(timerContent);
-    };
-  }, []);
-
-  // Maneja el clic del botón "¡Empezá acá!"
-  const handleButtonClick = () => {
-    setShowSecondaryScreen(true); // Muestra la segunda pantalla al hacer clic
-  };
 
   // Maneja el clic del botón "Ver mapa"
   const handleMapClick = () => {
-    setShowMap(true); // Muestra el mapa al hacer clic
+    setShowMap(prevState => !prevState); // Alterna entre mostrar y ocultar el mapa
   };
 
   return (
     <div className={styles.pageContainer}>
-      {!showSecondaryScreen ? (
-        // Pantalla de bienvenida
-        <div>
+      {!showMap ? (
+        // Pantalla inicial con el logo y los tres botones
+        <>
           <div className={styles.logoContainer}>
-            <Image
-              src="/img/LOGO.jpg"
-              alt="Logo"
-              layout="intrinsic"
-              width={360} 
-              height={180}
-            />
-          </div>
-          <div className={`${styles.homeContent} ${showContent ? styles.show : ''}`}>
-            <h1>Bienvenido a Hospiturn</h1>
-            <h4>Reserva tu turno médico</h4>
-            <div className={styles.container}>
-              <Boton sendText={"¡Empezá acá!"} type={"secondary"} onClick={handleButtonClick} />
+      <Image
+        src="/img/logos.png"
+        alt="Logo"
+        layout="intrinsic"
+        width={300} // Cambia el ancho según lo necesites
+        height={150} // Cambia la altura según lo necesites
+      />
+</div>
+
+          <div className={styles.buttonsContainer}>
+            <Link href="/informacion" passHref legacyBehavior>
+              <a className={styles.linkButton}>
+                Sacar Turno
+              </a>
+            </Link>
+
+            <div className={styles.nextTurnContainer}>
+              <span className={styles.nextTurnLabel}>Próximo turno en:</span>
+              <span className={styles.nextTurnValue}>---</span>
             </div>
+
+            <button className={styles.secondaryButton} onClick={handleMapClick}>
+              Ver mapa
+            </button>
           </div>
-        </div>
+        </>
       ) : (
-        // Segunda pantalla con la foto y los botones
-        <div className={styles.secondaryScreen}>
-          {!showMap ? (
-            // Vista inicial de la segunda pantalla sin el mapa
-            <div className={styles.buttonsContainer}>
-              <Link href="/informacion" passHref legacyBehavior>
-                <a className={styles.linkButton}>
-                  Sacar Turno
-                </a>
-              </Link>
-
-              <div className={styles.nextTurnContainer}>
-                <span className={styles.nextTurnLabel}>Próximo turno en:</span>
-                <span className={styles.nextTurnValue}>---</span>
-              </div>
-
-              <button className={styles.secondaryButton} onClick={handleMapClick}>
-                Ver mapa
-              </button>
-            </div>
-          ) : (
-            // Vista con el mapa
-            <div className={styles.mapContainer}>
-              <Image 
-                src="/img/mapa.png" 
-                alt="Mapa"
-                width={360}
-                height={200}
-                layout="responsive"
-              />
-            </div>
-          )}
-        </div>
+        // Vista con el mapa
+        <div className={styles.mapContainer}>
+        <Image 
+          src="/img/mapa.png" 
+          alt="Mapa"
+          width={360}
+          height={200}
+          layout="responsive"
+        />
+        <button className={styles.secondaryButton} onClick={handleMapClick}>
+          Volver
+        </button>
+      </div>
+      
       )}
     </div>
   );
