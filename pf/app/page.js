5f81062442266d5590039c23527f80df060a439d
Tu Nombre <tuemail@example.com>
2024-09-04 08:24:00 -0300

    terminada la home, mas prolijo el timer

--- pf/app/page.js
+++ pf/app/page.js
@@ -6,59 +6,67 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 const HomePage = () => {
-  const [showMap, setShowMap] = useState(false); // Estado para mostrar el mapa
+  const [showMap, setShowMap] = useState(false);
+  const [buttonsVisible, setButtonsVisible] = useState(false);
 
   const handleMapClick = () => {
-    setShowMap(prevState => !prevState); // Alterna entre mostrar y ocultar el mapa
+    setShowMap(prevState => !prevState); 
   };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setButtonsVisible(true);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <div className={styles.pageContainer}>
       {!showMap ? (
-        // Pantalla inicial con el logo y los tres botones
         <>
           <div className={styles.logoContainer}>
-      <Image
-        src="/img/logos.png"
-        alt="Logo"
-        layout="intrinsic"
-        width={300} // Cambia el ancho según lo necesites
-        height={150} // Cambia la altura según lo necesites
-      />
-</div>
-
-          <div className={styles.buttonsContainer}>
-            <Link href="/informacion" passHref legacyBehavior>
-              <a className={styles.linkButton}>
-                Sacar Turno
-              </a>
-            </Link>
-
-            <div className={styles.nextTurnContainer}>
-              <span className={styles.nextTurnLabel}>Próximo turno en:</span>
-              <span className={styles.nextTurnValue}>---</span>
-            </div>
-
-            <button className={styles.secondaryButton} onClick={handleMapClick}>
-              Ver mapa
-            </button>
+            <Image
+              src="/img/logos.png"
+              alt="Logo"
+              layout="intrinsic"
+              width={300} 
+              height={150} 
+            />
           </div>
+
+          {buttonsVisible && (
+            <div className={styles.buttonsContainer}>
+              <Link href="/informacion" passHref legacyBehavior>
+                <a className={styles.linkButton}>
+                  Sacar Turno
+                </a>
+              </Link>
+
+              <div className={styles.nextTurnContainer}>
+                <span className={styles.nextTurnLabel}>Próximo turno en:</span>
+                <span className={styles.nextTurnValue}>---</span>
+              </div>
+
+              <button className={styles.secondaryButton} onClick={handleMapClick}>
+                Ver mapa
+              </button>
+            </div>
+          )}
         </>
       ) : (
-        // Vista con el mapa
         <div className={styles.mapContainer}>
-        <Image 
-          src="/img/mapa.png" 
-          alt="Mapa"
-          width={360}
-          height={200}
-          layout="responsive"
-        />
-        <button className={styles.secondaryButton} onClick={handleMapClick}>
-          Volver
-        </button>
-      </div>
-      
+          <Image 
+            src="/img/mapa.png" 
+            alt="Mapa"
+            width={360}
+            height={200}
+            layout="responsive"
+          />
+          <button className={styles.secondaryButton} onClick={handleMapClick}>
+            Volver
+          </button>
+        </div>
       )}
     </div>
   );
